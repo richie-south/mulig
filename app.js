c3b5fa53cdef75aed40ed886a80eece33add3810
rs222kn <rs222kn@student.lnu.se>
2017-03-02 15:17:53 +0100

    Implemented inOrder function

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 'use strict'
 
-const mulig = (promises, fn = () => {}, errorFn = (message) => {}) => 
+
+/**
+ * runs fn on order of fastest to resolve
+ * @param  {Array}    promises [array of promise]
+ * @param  {Function} fn       [resolve function, this runs when a promise resolves]
+ * @param  {Function} errorFn  [error function, this runs when a promise throws an error]
+ * @return {Array}             [returns given array]
+ */
+const promiseMapFn = (promises, fn, errorFn) => 
   promises.map((item, index) => {
     item
       .then(value => fn(value, index))
@@ -8,4 +16,46 @@ const mulig = (promises, fn = () => {}, errorFn = (message) => {}) =>
     return item    
   })
 
+/**
+ * runs fn on order of fastest to resolve
+ * PARAMS CHECK: promiseMapFn 
+ */
+const mulig = (promises, fn = () => {}, errorFn = () => {}) => 
+    promiseMapFn(promises, fn, errorFn)
+
+/**
+ * runs fn in order of given array: as fast as posible
+ * PARAMS CHECK: promiseMapFn
+ */
+const inOrder = (promises, fn = () => {}, errorFn = () => {}) => {
+  
+  const queue = []
+  let queueToRun = []
+  
+  const runQueue = () => {
+    const queueIterator = queue[Symbol.iterator]()
+    
+    for(let item of queueIterator){
+      
+      if(!item){
+        break
+      }else if(!item.hasRun){
+        queue[item.index].hasRun = true
+        queueToRun.push(item)  
+      }
+    }
+    
+    queueToRun.forEach(({value, index}) => fn(value, index))
+    queueToRun = []
+  }
+  
+  const addToQueue = (value, index) => {
+    queue[index] = { value, index, hasRun: false }
+    runQueue()
+  }
+  
+  return promiseMapFn(promises, addToQueue, errorFn)
+}
+
 module.exports = mulig
+module.exports.inOrder = inOrder
