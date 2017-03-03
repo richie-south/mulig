5b868ca6d1caf03b8c4c756aca246af2ad8a0997
rs222kn <rs222kn@student.lnu.se>
2017-03-03 21:03:22 +0100

    Added stack function and more comments

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const mulig = (promises, fn = () => {}, errorFn = () => {}) =>
     promiseMapFn(promises, fn, errorFn)
 
 /**
+ * first in array: first to call fn
  * runs fn in order of given array: as fast as posible
  * PARAMS CHECK: promiseMapFn
  */
@@ -31,9 +32,17 @@ const queue = (promises, fn = () => {}, errorFn = () => {}) => {
   
   const queue = []
   
+  /**
+   * Runs queue in order of promises array
+   */
   const runQueue = () => {
     const queueIterator = queue[Symbol.iterator]()
     
+    /**
+     * array of items from queue to run
+     * @param {Iterator} queueItr [iterator]
+     * @return [a array of newly resoved item]
+     */
     const queueToRun = ((queueItr) => {
       const _queue = []
       for(let item of queueItr){
@@ -50,6 +59,12 @@ const queue = (promises, fn = () => {}, errorFn = () => {}) => {
     queueToRun.forEach(({value, index}) => fn(value, index))
   }
   
+  /**
+   * adds value, index to queue
+   * runs runQueue
+   * @param {any} value 
+   * @param {Number} index 
+   */
   const addToQueue = (value, index) => {
     queue[index] = { value, index, hasRun: false }
     runQueue()
@@ -58,8 +73,19 @@ const queue = (promises, fn = () => {}, errorFn = () => {}) => {
   return promiseMapFn(promises, addToQueue, errorFn)
 }
 
+/**
+ * Reverse queue
+ * last in array: first to call fn
+ * @return [Array of promises in same order as given]
+ */
+const stack = (promises, fn = () => {}, errorFn = () => {}) => 
+  queue(promises.reverse(), fn, errorFn).reverse()
+
 module.exports = mulig
 module.exports.queue = queue
+module.exports.stack = stack
+
+// deprecated
 module.exports.inOrder = (...args) => {
   console.log('.inOrder() is deprecated! Use .queue() instead!')
   return queue(...args)
