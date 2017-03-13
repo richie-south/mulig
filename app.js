339ad39ed615a829cdf4292b94ac969b520fd0a4
rs222kn <rs222kn@student.lnu.se>
2017-03-13 21:50:11 +0100

    cleened code

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const gSuccessFn = (value, index, isDone, prev) => prev
+const gErrorFn = (value, index, isDone, prev) => prev
+
 /**
  * runs fn on order of fastest to resolve
  * @param  {Array}    promises [array of promise]
@@ -27,20 +30,20 @@ const addIsDoneAndReturnedValue = (totalLength, successFn, errorFn) => {
   let nrOfRuns = 0
   let returnedValue
 
-  const _run = (fn) => (value, index) => {
+  const _ = fn => (value, index) => {
     nrOfRuns++
     const isEqualLength = nrOfRuns === totalLength
     returnedValue = fn(value, index, isEqualLength, returnedValue)
   }
 
-  return [_run(successFn), _run(errorFn)]
+  return [_(successFn), _(errorFn)]
 }
 
 /**
  * runs fn on order of fastest to resolve
  * PARAMS CHECK: promiseMapFn 
  */
-const mulig = (promises, successFn = () => {}, errorFn = () => {}) => {
+const mulig = (promises, successFn = gSuccessFn, errorFn = gErrorFn) => {
   const totalLength = promises.length
   const [_successFn, _errorFn] = addIsDoneAndReturnedValue(totalLength, successFn, errorFn)
   return promiseMapFn(promises, _successFn, _errorFn)
@@ -51,7 +54,7 @@ const mulig = (promises, successFn = () => {}, errorFn = () => {}) => {
  * runs fn in order of given array: as fast as posible
  * PARAMS CHECK: promiseMapFn
  */
-const queue = (promises, successFn = () => {}, errorFn = () => {}) => {
+const queue = (promises, successFn = gSuccessFn, errorFn = gErrorFn) => {
   
   const queue = []
   const [_successFn, _errorFn] = addIsDoneAndReturnedValue(promises.length, successFn, errorFn)
@@ -66,8 +69,8 @@ const queue = (promises, successFn = () => {}, errorFn = () => {}) => {
       if(!item){
         return
       }else if(!item.hasRun){
-        queue[item.index].hasRun = true
         const { fn, value, index } = item
+        queue[index].hasRun = true
         fn(value, index)
       }
     }
@@ -78,10 +81,11 @@ const queue = (promises, successFn = () => {}, errorFn = () => {}) => {
    * runs runQueue
    * @param {fn} [fn to pass in queue object]
    * @returns [Function] [function to take the rest params]
+   * 
    * @param {any} value 
    * @param {Number} index 
    */
-  const addToQueue = (fn) => (value, index) => {
+  const addToQueue = fn => (value, index) => {
     queue[index] = { value, index, hasRun: false, fn }
     runQueue()
   }
@@ -94,7 +98,7 @@ const queue = (promises, successFn = () => {}, errorFn = () => {}) => {
  * last in array: first to call fn
  * @return [Array of promises in same order as given]
  */
-const stack = (promises, successFn = () => {}, errorFn = () => {}) => 
+const stack = (promises, successFn = gSuccessFn, errorFn = gErrorFn) => 
   queue(promises.reverse(), successFn, errorFn).reverse()
 
 module.exports = mulig
